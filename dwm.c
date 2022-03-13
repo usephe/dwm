445c9f5592d464770c63dd7c0b1c56d3c9391e3c
Usef El <usefel@pm.me>
2022-03-13 13:01:33 +0100

    Do not allow the focus to drift from the active fullscreen client when using focusstack()

--- dwm.c
+++ dwm.c
@@ -1722,7 +1722,7 @@ toggletag(const Arg *arg)
 {
 	unsigned int newtags;
 
-	if (!selmon->sel)
+	if (!selmon->sel || selmon->sel->isfullscreen)
 		return;
 	newtags = selmon->sel->tags ^ (arg->ui & TAGMASK);
 	if (newtags) {
