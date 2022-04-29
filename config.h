c9f6693f6f76bcfdc7e8a8c08235f93de0a1c37d
Usef El <usefel@pm.me>
2022-04-29 09:32:43 +0000

    feat: change color scheme
    Add a new gray color and change the normal statusbar background color

--- config.h
+++ config.h
@@ -10,6 +10,7 @@ static const char *fonts[]          = {
 	"NotoColorEmoji:pixelsize=14:antialias=true:autohint=true",
 	"DejaVu Sans Mono Nerd Font:pixelsize=16:antialias=true:autohint=true"
 };
+static const char col_gray0[]       = "#181818";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -17,7 +18,7 @@ static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeNorm] = { col_gray3, col_gray0, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
